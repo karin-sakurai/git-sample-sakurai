5ebc7b3bfbcd7859c68d99d3ebe30372b74faaca
sakuraikarin <karin.sakurai@rakus-partners.co.jp>
2022-05-10 14:33:38 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleSakurai1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleSakurai1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleSakurai1Application.class, args);
+	}
+
+}
